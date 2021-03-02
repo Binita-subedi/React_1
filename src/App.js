9318db7e1702522abddf21ca0475c950ed4723e9
Binita-subedi <subedibinita94@gmail.com>
2021-03-02 20:49:38 +0545

    Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import FragmentDemo from './Component/FragmentDemo'
+import Table from './Component/Table'
+import PureComp from './Component/PureComp'
+import ParentComp from './Component/ParentComp'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div classname="App">
+        {/* <Table /> */}
+        {/* <FragmentDemo /> */}
+        <ParentComp />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
+
