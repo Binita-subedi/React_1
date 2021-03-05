34ddaf4924805fd284d9d1c639ba39cc6816a0a5
Binita-subedi <subedibinita94@gmail.com>
2021-03-05 13:47:58 +0545

    RefsDemo

--- src/Component/RefsDemo.js
+++ src/Component/RefsDemo.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
 
 class RefsDemo extends Component {
+  constructor(props) {
+    super(props)
+    this.inputRef = React.createRef()
+  }
+  
   render() {
     return (
       <div>
